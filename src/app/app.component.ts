c9bd76495c4fdf349dca7f6884c0b25c3c9c6d8f
GangadharJannu <gangadhar014@gmail.com>
2019-07-05 13:03:52 +0530

    added chart functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'csv-to-chart';
+  public chartData;
+  fileInput(files: FileList): void {
+    this.readFile(files[0]).then(success => {
+      this.chartData = this.CSVToObj(success);
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  readFile(file: File): Promise<string> {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onload = () => {
+        resolve(reader.result.toString());
+      };
+      reader.onerror = (event) => {
+        reject(reader.error);
+      };
+      reader.readAsText(file);
+    });
+  }
+
+  CSVToObj(CSVString: string): object {
+    const labels = new Set();
+    const datasets = CSVString
+      .split(/\r\n|\n/)
+      .map(rows => {
+        const cols = rows.split(',');
+        return {
+          label: cols[0],
+          data: cols.slice(1).map(dataCols => {
+            const obj = dataCols.split('|');
+            labels.add(obj[0]);
+            return { x: obj[0], y: obj[1] };
+          }),
+          borderColor: '#' + Math.floor(Math.random() * 16777215).toString(16)
+        };
+      });
+    return { labels: Array.from(labels).sort(), datasets };
+  }
 }
--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ChangeDetectorRef, Input } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-chart',
   templateUrl: './chart.component.html',
   styleUrls: ['./chart.component.scss']
 })
-export class ChartComponent implements OnInit {
+export class ChartComponent implements AfterViewInit {
+  private ctx;
+  private labels: Array<string>;
+  private datasets;
+  public chart: Chart;
 
-  constructor() { }
-
-  ngOnInit() {
+  @ViewChild('canvas', { static: true }) private canvas;
+  @Input() set dataObj(chartObj) {
+    console.log(chartObj);
+    if (chartObj) {
+      // tslint:disable-next-line:no-unused-expression
+      this.chart && this.chart.destroy();
+      this.labels = chartObj.labels;
+      this.datasets = chartObj.datasets.slice(0);
+      this.drawChart();
+    }
   }
 
+  constructor(private cdr: ChangeDetectorRef) { }
+
+  ngAfterViewInit() {
+    // this.drawChart();
+    // this.cdr.detectChanges();
+  }
+  drawChart() {
+    this.ctx = this.canvas.nativeElement.getContext('2d');
+    this.chart = new Chart(this.ctx, {
+      type: 'line',
+      data: {
+        labels: this.labels,
+        datasets: this.datasets
+      }
+    });
+  }
 }
